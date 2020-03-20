286251bcb7a6260361fef531ab57f71fd1138d67
Patricia Sittikul <pas217@lehigh.edu>
2020-03-20 19:10:00 -0400

    Messing with styles

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 
 <head>
     <title>Quarantine Keeper</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
     <!-- Google Fonts link -->
@@ -11,10 +14,37 @@
 </head>
 
 <body>
-    <h1 id="headerTitle">Quarantine Keeper</h1>
-    <p>Because you'll go fucking insane otherwise lmao </p>
+    <div class="container">
+        <!-- As a link -->
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <h1>Quarantine Keeper</h1>
+            </a>
+        </nav>
+
+        <!-- As a heading -->
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+        </nav>
+        <p>Because you'll go fucking insane otherwise lmao </p>
+
+        <div class="row">
+            <div class="col-sm">
+                # of Conversations (out loud):
+            </div>
+            <div class="col-sm">
+                # of Mental Breaks:
+            </div>
+            <div class="col-sm">
+                # of Existential Crises:
+            </div>
+        </div>
+    </div>
+
     <!-- jQuery CDN -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <!-- Your scripts question mark? -->
+    <!-- BootStrap scripts -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
