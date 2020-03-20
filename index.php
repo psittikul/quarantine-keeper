453b45bdddc18c35eb625afe419692f980b0d624
Patricia Sittikul <pas217@lehigh.edu>
2020-03-20 18:50:28 -0400

    Messing with styles

--- index.php
+++ index.php
@@ -3,15 +3,20 @@
 
 <head>
     <title>Quarantine Keeper</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
     <!-- Google Fonts link -->
-    <link href="https://fonts.googleapis.com/css?family=Baloo+Chettan+2|Bellota+Text|Lato|Muli|Work+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bellota+Text|Lato|Muli|Sen|Work+Sans&display=swap" rel="stylesheet">
 
 </head>
 
 <body>
-    <h1>Quarantine Keeper</h1>
+    <h1 id="headerTitle">Quarantine Keeper</h1>
     <p>Because you'll go fucking insane otherwise lmao </p>
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </body>
 
