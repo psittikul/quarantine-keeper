b53fe6c1c4f703dc23003e2e0ee6385bf7822bad
Patricia Sittikul <pas217@lehigh.edu>
2020-04-02 20:47:51 -0400

    Working on med tracker

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,18 @@ var dayMap = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+/**
+ * Event handlers
+ */
+$("#medsModal").on("click", ".med-btn", function () {
+    // Mark med as taken or not taken, accordingly
+    $(this).attr("data-taken", function () {
+        return $(this).attr("data-taken") === "no" ? "yes" : "no";
+    });
+});
+
+
 function getDisplayDate() {
     var today = new Date();
     var displayDate = "Today is: " + dayMap[today.getDay()] + ", " + monthMap[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
